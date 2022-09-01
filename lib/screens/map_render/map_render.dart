2e764db740fe41e7796d32a2e27622ca60dcfe5c
Medeiros <hsymetry@gmail.com>
2022-08-31 22:58:55 -0300

    Add keyboardMoviment and enemies as objects on map

--- lib/screens/map_render/map_render.dart
+++ lib/screens/map_render/map_render.dart
@@ -1,28 +1,46 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
+import 'package:pacman_game/utils/basic_values.dart';
+import 'package:pacman_game/widgets/enemy/necro_enemy.dart';
 import 'package:pacman_game/widgets/player/player.dart';
 
 class MapRender extends StatelessWidget {
   const MapRender({Key? key}) : super(key: key);
 
-  final double tileSize = 32;
-
   @override
   Widget build(BuildContext context) {
+    const tileSize = BasicValues.TILE_SIZE;
     return BonfireWidget(
+      // showCollisionArea: true,
       cameraConfig: CameraConfig(
         smoothCameraEnabled: true,
         moveOnlyMapArea: true,
-      ),
-      joystick: Joystick(
-        directional: JoystickDirectional(
-          size: 150,
-          isFixed: true,
+        zoom: 2.5,
+        sizeMovementWindow: Vector2(
+          tileSize * 3,
+          tileSize * 3,
         ),
       ),
-      map: WorldMapByTiled('map/pacmap.json',
-          forceTileSize: Vector2(tileSize, tileSize)),
-      player: GamePlayer(position: Vector2(25 * tileSize, 20 * tileSize)),
+      joystick: Joystick(
+        keyboardConfig: KeyboardConfig(
+            keyboardDirectionalType: KeyboardDirectionalType.wasdAndArrows),
+        directional: JoystickDirectional(
+          size: 110,
+          isFixed: true,
+          color: Colors.blueGrey.shade800,
+        ),
+      ),
+      map: WorldMapByTiled(
+        'map/pacmap.json',
+        objectsBuilder: {
+          'necro': (properties) => NecroEnemy(
+                position: properties.position,
+              ),
+        },
+      ),
+      player: GamePlayer(
+        position: Vector2(25 * tileSize, 20 * tileSize),
+      ),
     );
   }
 }
