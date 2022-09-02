9eebe9988b5c4c18e392ccfe10943917be33755c
Medeiros <hsymetry@gmail.com>
2022-09-02 17:55:28 -0300

    Add randomMoviment to enemy

--- lib/widgets/enemy/necro_enemy.dart
+++ lib/widgets/enemy/necro_enemy.dart
@@ -1,15 +1,17 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:pacman_game/utils/basic_values.dart';
+import 'package:pacman_game/widgets/coins/coins.dart';
 import 'package:pacman_game/widgets/enemy/necro_enemy_sprite_sheet.dart';
 
 const tileSize = BasicValues.TILE_SIZE;
 bool canMove = true;
 
-class NecroEnemy extends SimpleEnemy with ObjectCollision {
+class NecroEnemy extends SimpleEnemy
+    with ObjectCollision, AutomaticRandomMovement {
   NecroEnemy({required Vector2 position})
       : super(
           position: position,
-          speed: 50,
+          speed: totalCoins == 20 ? 50 : 40,
           size: Vector2(tileSize, tileSize),
           animation: SimpleDirectionAnimation(
             idleRight: NecroEnemySpriteSheet.enemyIdleRight,
@@ -35,23 +37,32 @@ class NecroEnemy extends SimpleEnemy with ObjectCollision {
   @override
   void update(double dt) {
     if (canMove) {
-      seeAndMoveToPlayer(
-        //Quando tiver próximo do player, faz:
-        closePlayer: (player) {
-          simpleAttackMelee(
-            damage: 20,
-            size: Vector2(tileSize, tileSize),
-            withPush: true,
-            animationRight: NecroEnemySpriteSheet.atackRight,
+      seePlayer(
+        observed: (player) {
+          seeAndMoveToPlayer(
+            //Quando tiver próximo do player, faz:
+            closePlayer: (player) {
+              simpleAttackMelee(
+                damage: totalCoins == 20 ? 30 : 20,
+                size: Vector2(tileSize, tileSize),
+                withPush: true,
+                animationRight: NecroEnemySpriteSheet.atackRight,
+              );
+            },
+            radiusVision: tileSize * 3,
+            margin: 4,
           );
         },
+        notObserved: () {
+          runRandomMovement(dt);
+        },
         radiusVision: tileSize * 3,
-        margin: 4,
       );
     }
     super.update(dt);
   }
 
+//Death
   @override
   void die() {
     gameRef.add(
@@ -61,6 +72,7 @@ class NecroEnemy extends SimpleEnemy with ObjectCollision {
         size: Vector2.all(tileSize),
       ),
     );
+    canMove = false;
     removeFromParent();
     super.die();
   }
