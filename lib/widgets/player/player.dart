345f8c96322865217d71f86dd680a223c18b4c50
Medeiros <hsymetry@gmail.com>
2022-09-01 22:46:03 -0300

    Add death, grave and lifeBar

--- lib/widgets/player/player.dart
+++ lib/widgets/player/player.dart
@@ -1,10 +1,13 @@
 import 'package:bonfire/bonfire.dart';
+import 'package:flutter/material.dart';
 import 'package:pacman_game/utils/basic_values.dart';
 import 'package:pacman_game/widgets/player/player_sprite_sheet.dart';
 
 const tileSize = BasicValues.TILE_SIZE;
 
 class GamePlayer extends SimplePlayer with ObjectCollision {
+  bool canMove = true;
+
   GamePlayer({required Vector2 position})
       : super(
           position: position,
@@ -29,5 +32,54 @@ class GamePlayer extends SimplePlayer with ObjectCollision {
     );
   }
 
-  //Método onColision
+//Life bar
+  @override
+  void render(Canvas canvas) {
+    drawDefaultLifeBar(
+      canvas,
+      width: 10,
+      borderWidth: 1,
+      height: 1.5,
+      align: const Offset(3, -5),
+      borderRadius: BorderRadius.circular(3),
+    );
+
+    super.render(canvas);
+  }
+
+  //Receive damage
+  @override
+  void receiveDamage(AttackFromEnum attacker, double damage, identify) {
+    showDamage(
+      damage,
+      direction: DirectionTextDamage.RANDOM,
+      config: const TextStyle(
+        fontSize: 5,
+        color: Colors.white,
+        fontFamily: 'Normal',
+      ),
+    );
+    super.receiveDamage(attacker, damage, identify);
+  }
+
+  @override
+  void die() async {
+    if (lastDirectionHorizontal == Direction.left) {
+      animation?.playOnce(PlayerSpriteSheet.dyingLeft,
+          onFinish: () => removeFromParent());
+    } else {
+      animation?.playOnce(PlayerSpriteSheet.dyingRight,
+          onFinish: () => removeFromParent());
+    }
+
+    final sprite = await PlayerSpriteSheet.simpleGrave;
+    gameRef.add(
+      GameDecoration.withSprite(
+        sprite: sprite.getSprite(),
+        position: Vector2(position.x, position.y),
+        size: Vector2.all(tileSize),
+      ),
+    );
+    super.die();
+  }
 }
--- lib/widgets/player/player_sprite_sheet.dart
+++ lib/widgets/player/player_sprite_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:bonfire/bonfire.dart';
+import 'package:pacman_game/widgets/player/player.dart';
 
 class PlayerSpriteSheet {
   static Future<SpriteAnimation> get playerIdleLeft => SpriteAnimation.load(
@@ -35,6 +36,57 @@ class PlayerSpriteSheet {
           stepTime: 0.15,
           textureSize: Vector2(24, 24),
           texturePosition: Vector2(0, 48),
+        ),
+      );
+
+  //ReceiveDamage
+  // static Future<SpriteAnimation> get receiveDamageLeft => SpriteAnimation.load(
+  //       'fHero_.png',
+  //       SpriteAnimationData.sequenced(
+  //         amount: 4,
+  //         stepTime: 0.15,
+  //         textureSize: Vector2(24, 24),
+  //         texturePosition: Vector2(96, 24),
+  //       ),
+  //     );
+  // static Future<SpriteAnimation> get receiveDamageRight => SpriteAnimation.load(
+  //       'fHero_.png',
+  //       SpriteAnimationData.sequenced(
+  //         amount: 4,
+  //         stepTime: 0.15,
+  //         textureSize: Vector2(24, 24),
+  //         texturePosition: Vector2(0, 96),
+  //       ),
+  //     );
+
+  //Dying
+  static Future<SpriteAnimation> get dyingLeft => SpriteAnimation.load(
+        'fHero_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.15,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(120, 24),
+        ),
+      );
+  static Future<SpriteAnimation> get dyingRight => SpriteAnimation.load(
+        'fHero_.png',
+        SpriteAnimationData.sequenced(
+          amount: 4,
+          stepTime: 0.15,
+          textureSize: Vector2(24, 24),
+          texturePosition: Vector2(96, 120),
+        ),
+      );
+
+  //Grave
+  static Future<SpriteAnimation> get simpleGrave => SpriteAnimation.load(
+        'fHero_die.png',
+        SpriteAnimationData.sequenced(
+          amount: 1,
+          stepTime: 0.15,
+          textureSize: Vector2(tileSize, tileSize),
+          texturePosition: Vector2(0, 0),
         ),
       );
 }
