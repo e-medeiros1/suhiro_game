bcac870141d638fa72eff5f19cb2ab83430a9b55
Medeiros <hsymetry@gmail.com>
2022-09-01 22:44:43 -0300

    Add new animation for enemy

--- lib/widgets/enemy/necro_enemy_sprite_sheet.dart
+++ lib/widgets/enemy/necro_enemy_sprite_sheet.dart
@@ -37,4 +37,47 @@ class NecroEnemySpriteSheet {
           texturePosition: Vector2(0, 24),
         ),
       );
+
+//Death
+  static Future<SpriteAnimation> get smokeExplosion => SpriteAnimation.load(
+        'smoke_explosion.png',
+        SpriteAnimationData.sequenced(
+          amount: 6,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+
+  static Future<SpriteAnimation> get atackLeft => SpriteAnimation.load(
+        'atack_effect_left.png',
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+  static Future<SpriteAnimation> get atackRight => SpriteAnimation.load(
+        'atack_effect_right.png',
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+  static Future<SpriteAnimation> get atackUp => SpriteAnimation.load(
+        'atack_effect_up.png',
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+  static Future<SpriteAnimation> get atackDown => SpriteAnimation.load(
+        'atack_effect_down.png',
+        SpriteAnimationData.sequenced(
+          amount: 3,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
 }
