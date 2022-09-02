18033211a837bd24d1ea99b415e3775857a3eb9e
Medeiros <hsymetry@gmail.com>
2022-09-02 17:53:34 -0300

    Add new objects

--- lib/screens/map_render/map_render.dart
+++ lib/screens/map_render/map_render.dart
@@ -1,6 +1,8 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
 import 'package:pacman_game/utils/basic_values.dart';
+import 'package:pacman_game/widgets/coins/coins.dart';
+import 'package:pacman_game/widgets/decoration/board.dart';
 import 'package:pacman_game/widgets/decoration/bonfire.dart';
 import 'package:pacman_game/widgets/enemy/necro_enemy.dart';
 import 'package:pacman_game/widgets/player/player.dart';
@@ -12,16 +14,14 @@ class MapRender extends StatelessWidget {
   Widget build(BuildContext context) {
     const tileSize = BasicValues.TILE_SIZE;
     return BonfireWidget(
-      // showCollisionArea: true,
+//Camera
       cameraConfig: CameraConfig(
         smoothCameraEnabled: true,
         moveOnlyMapArea: true,
         zoom: 2.3,
-        sizeMovementWindow: Vector2(
-          tileSize * 3,
-          tileSize * 3,
-        ),
+        sizeMovementWindow: Vector2(tileSize * 5, tileSize * 5),
       ),
+//Joystic
       joystick: Joystick(
         keyboardConfig: KeyboardConfig(
             keyboardDirectionalType: KeyboardDirectionalType.wasdAndArrows),
@@ -31,20 +31,21 @@ class MapRender extends StatelessWidget {
           color: Colors.blueGrey.shade800,
         ),
       ),
+//Map Render
       map: WorldMapByTiled(
         'map/pacmap.json',
         objectsBuilder: {
-          'necro': (properties) => NecroEnemy(
-                position: properties.position,
-              ),
-          'bonfire': (properties) => Lamp(
-                position: properties.position,
-              ),
+          'necro': (properties) => NecroEnemy(position: properties.position),
+          'bonfire': (properties) => Lamp(position: properties.position),
+          'coin': (properties) => Coins(position: properties.position),
+          'board': (properties) => Board(position: properties.position),
         },
       ),
+//Player
       player: GamePlayer(
-        position: Vector2(25 * tileSize, 20 * tileSize),
+        position: Vector2(tileSize * 25, tileSize * 20),
       ),
+//Lighting
       lightingColorGame: Colors.black.withOpacity(0.15),
     );
   }
