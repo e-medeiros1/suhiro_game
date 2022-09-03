a12be38b207c4e7d3bb3493d6261148b24d5c56f
Medeiros <hsymetry@gmail.com>
2022-09-03 03:04:52 -0300

    Increase speed coin animation

--- lib/widgets/coins/coins_sprite_sheet.dart
+++ lib/widgets/coins/coins_sprite_sheet.dart
@@ -1,12 +1,11 @@
 import 'package:bonfire/bonfire.dart';
 
 class CoinSpriteSheet {
-
-       static Future<SpriteAnimation> get coins => SpriteAnimation.load(
+  static Future<SpriteAnimation> get coins => SpriteAnimation.load(
         'coin_sheet.png',
         SpriteAnimationData.sequenced(
           amount: 4,
-          stepTime: 0.1,
+          stepTime: 0.15,
           textureSize: Vector2(8, 8),
           texturePosition: Vector2(0, 0),
         ),
