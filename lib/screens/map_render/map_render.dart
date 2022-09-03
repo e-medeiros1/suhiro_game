86597fac5a23c5456a5aebda4f2dacbb69b9ac7b
Medeiros <hsymetry@gmail.com>
2022-09-03 01:32:29 -0300

    Set listener for game over and restart

--- lib/screens/map_render/map_render.dart
+++ lib/screens/map_render/map_render.dart
@@ -1,19 +1,35 @@
 import 'package:bonfire/bonfire.dart';
 import 'package:flutter/material.dart';
 import 'package:pacman_game/utils/basic_values.dart';
+import 'package:pacman_game/utils/game_controller.dart';
 import 'package:pacman_game/widgets/coins/coins.dart';
 import 'package:pacman_game/widgets/decoration/board.dart';
 import 'package:pacman_game/widgets/decoration/bonfire.dart';
 import 'package:pacman_game/widgets/enemy/necro_enemy.dart';
 import 'package:pacman_game/widgets/player/player.dart';
 
-class MapRender extends StatelessWidget {
+class MapRender extends StatefulWidget {
   const MapRender({Key? key}) : super(key: key);
+
+  @override
+  State<MapRender> createState() => _MapRenderState();
+}
+
+class _MapRenderState extends State<MapRender> implements GameListener{
+
+  late  GameController controller;
+
+  @override
+  void initState() {
+  controller = GameController()..addListener(this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     const tileSize = BasicValues.TILE_SIZE;
     return BonfireWidget(
+      gameController: controller,
 //Camera
       cameraConfig: CameraConfig(
         smoothCameraEnabled: true,
@@ -21,15 +37,29 @@ class MapRender extends StatelessWidget {
         zoom: 2.3,
         sizeMovementWindow: Vector2(tileSize * 5, tileSize * 5),
       ),
+           components: [
+        MyGameController(),
+        
+      ],
 //Joystic
       joystick: Joystick(
         keyboardConfig: KeyboardConfig(
             keyboardDirectionalType: KeyboardDirectionalType.wasdAndArrows),
         directional: JoystickDirectional(
+          spriteBackgroundDirectional: Sprite.load('joystick_background.png'),
+          spriteKnobDirectional: Sprite.load('joystick_knob.png'),
           size: 110,
-          isFixed: true,
-          color: Colors.blueGrey.shade800,
+          // color: Colors.blueGrey.shade800,
         ),
+        actions: [
+          JoystickAction(
+            actionId: 0,
+            sprite: Sprite.load('joystick_atack.png'),
+            spritePressed: Sprite.load('joystick_atack_selected.png'),
+            size: 80,
+            margin: const EdgeInsets.only(bottom: 50, right: 80),
+          ),
+        ],
       ),
 //Map Render
       map: WorldMapByTiled(
@@ -48,5 +78,15 @@ class MapRender extends StatelessWidget {
 //Lighting
       lightingColorGame: Colors.black.withOpacity(0.15),
     );
+  }
+  
+  @override
+  void changeCountLiveEnemies(int count) {
+    // TODO: implement changeCountLiveEnemies
+  }
+  
+  @override
+  void updateGame() {
+    // TODO: implement updateGame
   }
 }
